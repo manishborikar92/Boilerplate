8ee764fd36239d30cc9503f52e6ffb7f8a2afd8b
Manish Borikar <manishborikar92@gmail.com>
2026-05-10 13:43:26 +0530

    chore: remove storage-module directory and its contents
    This module was extracted for potential reuse but is no longer needed in this repository. The functionality may be maintained elsewhere or will be re-integrated differently in the future.

--- storage-module/src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './storage-error.js';
-export * from './storage.adapter.js';
-export * from './adapters/cloudinary-storage.adapter.js';
--- storage-module/src/storage-error.js
+++
@@ -1,8 +0,0 @@
-export class StorageModuleError extends Error {
-  constructor(message, { statusCode = 500, providerCode = null } = {}) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = statusCode;
-    this.providerCode = providerCode;
-  }
-}
